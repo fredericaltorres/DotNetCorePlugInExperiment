edf939d37442341d0e61f96e20582770022cb094
Frederic Torres <fredericaltorres@gmail.com>
2019-05-01 17:50:23 -0400

    Update comment

--- DotNetCorePlugInExperimentConsole/Program.cs
+++ DotNetCorePlugInExperimentConsole/Program.cs
@@ -14,13 +14,15 @@ namespace DotNetCoreConsoleTestAutoInstaller
         {
             Console.WriteLine("START!");
 
+            // Load assembly from the file system
             var assemblyFileName = @"C:\DVT\.NET\DotNetCorePlugInExperiment\FredCoreLib\bin\Debug\netcoreapp2.1\FredCoreLib.dll";
             var myPlugIn = new PlugInManager(assemblyFileName, "FredCoreLib.FredPlugIn").Load();
-            Console.WriteLine($"Toto:{myPlugIn.Instance.Run("myParam1")}");
+            Console.WriteLine($"Execute PlugIn Instance :{myPlugIn.Instance.Run("myParam1")}");
 
+            // Load assembly as binary
             byte[] assemblyBinary = File.ReadAllBytes(assemblyFileName);
             var myPlugIn2 = new PlugInManager(assemblyBinary, "FredCoreLib.FredPlugIn").Load();
-            Console.WriteLine($"Toto:{myPlugIn2.Instance.Run("myParam1")}");
+            Console.WriteLine($"Execute PlugIn Instance :{myPlugIn2.Instance.Run("myParam1")}");
 
             Console.ReadKey();
         }
