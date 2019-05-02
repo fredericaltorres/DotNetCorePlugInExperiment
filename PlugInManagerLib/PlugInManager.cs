c21e9c88d55fd98e68f5ed35b06b59c0ac63ec4b
Frederic Torres <fredericaltorres@gmail.com>
2019-05-01 22:37:57 -0400

    Simplify PlugInManager

--- PlugInManagerLib/PlugInManager.cs
+++ PlugInManagerLib/PlugInManager.cs
@@ -15,39 +15,39 @@ namespace PlugInManagerLib
         private Type _plugInType;
         private IPlugIn _plugIn = null;
 
-        public PlugInManager(string assemblyFileName, string plugInFQN)
+        public PlugInManager( string plugInFQN)
         {
-            LoadAssembly(assemblyFileName);
             this._plugInFQN = plugInFQN;
         }
 
-        public PlugInManager(byte [] assemblyBinary, string plugInFQN)
+        public PlugInManager(string assemblyFileName, string plugInFQN) : this(plugInFQN)
         {
-            LoadAssembly(assemblyBinary);
-            this._plugInFQN = plugInFQN;
+            byte[] assemblyBinary = null;
+            try
+            {
+                assemblyBinary = File.ReadAllBytes(assemblyFileName);
+            }
+            catch (System.IO.FileNotFoundException fnfex)
+            {
+                throw new PlugInManagerException($"Cannot load assembly {assemblyFileName}", fnfex);
+            }
+            this.LoadAssembly(assemblyBinary);
+        }
+
+        public PlugInManager(byte [] assemblyBinary, string plugInFQN) : this(plugInFQN)
+        {
+            this.LoadAssembly(assemblyBinary);
         }
 
         private void LoadAssembly(byte [] buffer)
         {
             // Is it possible to unload an Assembly loaded dynamically in dotnet core?
             // https://stackoverflow.com/questions/43106917/is-it-possible-to-unload-an-assembly-loaded-dynamically-in-dotnet-core
-
-            MemoryStream stream = new MemoryStream(buffer);
             if(this._assembly == null)
+            {
+                var stream = new MemoryStream(buffer);
                 this._assembly = AssemblyLoadContext.Default.LoadFromStream(stream);
-        }
-
-        private void LoadAssembly(string assemblyFileName)
-        {
-            try
-            {
-                var assemblyBinary = File.ReadAllBytes(assemblyFileName);
-                this.LoadAssembly(assemblyBinary);
-            }
-            catch (System.Exception ex)
-            {
-                throw new PlugInManagerException($"Cannot load assembly {assemblyFileName}", ex);
-            }
+            }                
         }
 
         public bool Load()
@@ -59,27 +59,16 @@ namespace PlugInManagerLib
             }
             catch
             {
-
             }
             return false;
         }
 
-        public List<string> GetTypes()
-        {
-            var l = new List<string>();
-            foreach (TypeInfo type in this._assembly.DefinedTypes)
-            {
-                l.Add(type.FullName);
-            }
-            return l;
-        }
 
         public bool IsValidPlugIn
         {
             get
             {
                 // https://stackoverflow.com/questions/4963160/how-to-determine-if-a-type-implements-an-interface-with-c-sharp-reflection
-    
                 return typeof(IPlugIn).IsAssignableFrom(this._plugInType);
             }
         }
@@ -119,6 +108,20 @@ namespace PlugInManagerLib
                     throw new PlugInManagerException($"PlugIn not loaded");
                 return this._plugIn;
             }
-        }        
+        }
+
+        /// <summary>
+        /// TODO: Get rid of it
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetTypes()
+        {
+            var l = new List<string>();
+            foreach (TypeInfo type in this._assembly.DefinedTypes)
+            {
+                l.Add(type.FullName);
+            }
+            return l;
+        }
     }
 }
